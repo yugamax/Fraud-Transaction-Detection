fb7402f66405f3ef8b0e1b26810fa9574ed47be2
yugamax <ayp14111@gmail.com>
2025-05-29 21:24:10 +0530

    Add pandas import and update prediction logic with dataset checks

--- api_connect.py
+++ api_connect.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import numpy as np
+import pandas as pd
 import joblib
 import uvicorn
 import os
@@ -17,6 +18,7 @@ app.add_middleware(
 )
 
 model = joblib.load(r"model/xgb_model.joblib")
+df = pd.read_csv("dataset/transaction_recs.csv")
 
 class Trans_data(BaseModel):
     features: list[float]
@@ -25,14 +27,26 @@ class Trans_data(BaseModel):
 async def predict(data: Trans_data):
 
     if len(data.features) != 18:
+        print("Missing features. Expected 18 features.")
         return {"ML error": "Missing features. Expected 18 features."}
 
     input_data = np.array(data.features).reshape(1, -1)
     prediction = model.predict(input_data)[0]
 
-    
-    label = "Fraud" if prediction == 1 else "Non - Fraud"
-    print(f"Prediction: {prediction}, Label: {label}")
+    if prediction == 1:
+        label = "Fraud"
+        row_exists = ((df.iloc[:, 2:20] == data.features).all(axis=1)).any()
+        if row_exists:
+            print("Row is present in the dataset so no changes made.")
+        else:
+            print("Row is not present in the dataset so updating the csv file.")
+            new_row = 
+            df.loc[len(df)] = new_row
+
+    else:
+        label = "Non - Fraud"
+
+
     return {
         "prediction": float(prediction),
         "label": label
