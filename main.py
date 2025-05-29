1033166e9f60dd4283eb76c743e5e5d0fcc6a568
yugamax <ayp14111@gmail.com>
2025-05-29 18:19:10 +0530

    Add classification report and accuracy output to fraud detection model

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
 from xgboost import XGBClassifier
 
 df = pd.read_csv(r"dataset\transaction_recs.csv")
@@ -16,4 +17,6 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_
 
 model = XGBClassifier(scale_pos_weight=balancingf)
 model.fit(x_train, y_train)
-y_pred = model.predict(x_test)
+y_pred = model.predict(x_test) 
+print(classification_report(y_test, y_pred))
+print("Accuracy:", model.score(x_test, y_test))
