be1c571ba4c95787cd47e6772956b833b98bb306
yugamax <ayp14111@gmail.com>
2025-05-29 18:20:06 +0530

    Refactor accuracy output formatting and add newline for better readability

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 from xgboost import XGBClassifier
 
+
 df = pd.read_csv(r"dataset\transaction_recs.csv")
 
 x = df.iloc[:,2:20]
@@ -13,10 +14,13 @@ y = y.replace({'Fraud': 1, 'Non - Fraud': 0})
 
 balancingf = y.value_counts()[0]/y.value_counts()[1]
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42, stratify=y)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.30, random_state=42, stratify=y)
 
 model = XGBClassifier(scale_pos_weight=balancingf)
 model.fit(x_train, y_train)
+
 y_pred = model.predict(x_test) 
+
 print(classification_report(y_test, y_pred))
-print("Accuracy:", model.score(x_test, y_test))
+
+print(f"Accuracy of model : {model.score(x_test, y_test)*100:.2f} %")
