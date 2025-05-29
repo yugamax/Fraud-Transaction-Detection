ea3a883cbe9e595ff05ae1a61db0728b71be680d
yugamax <ayp14111@gmail.com>
2025-05-30 04:48:10 +0530

    Refactor model loading and dataset handling; update prediction logic with improved confidence thresholds

--- api_connect.py
+++ api_connect.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+from typing import Union
 import numpy as np
 import pandas as pd
 import joblib
@@ -18,18 +19,22 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-model = joblib.load(r"model/xgb_model.joblib")
-df = pd.read_csv("dataset/transaction_recs.csv")
-df2 = pd.read_csv("dataset/mildly_unsafe_transactions.csv")
+pack = joblib.load(r"model\models.joblib")
+model = pack['model']
+enc1 = pack['enc1']
+enc2 = pack['enc2']
+
+df = pd.read_csv(r"dataset\cleaned_dataset.csv")
+df2 = pd.read_csv(r"dataset/mildly_unsafe_transactions.csv")
 
 class Transaction_data(BaseModel):
     acc_holder: str
-    features: list[float]
+    features: list[Union[float , str]]
 
 def changes_in_dataset(label, data):
     new_row = [len(df)] + [label] + list(data) + [datetime.now().strftime("%d-%m-%Y %H:%M:%S")]
     df.loc[len(df)] = new_row
-    df.to_csv("dataset/transaction_recs.csv", index=False)
+    df.to_csv(r"dataset\cleaned_dataset.csv", index=False)
 
 @app.api_route("/ping", methods=["GET", "HEAD"])
 async def ping():
@@ -44,11 +49,14 @@ async def predict(data: Transaction_data):
         print("Missing features. Expected 18 features.")
         return {"ML error": "Missing features. Expected 18 features."}
 
+    data[-2] = enc1.transform(str([data[-2]]))[0]
+    data[-1] = enc2.transform(str([data[-1]]))[0]
+
     input_data = np.array(data).reshape(1, -1)
     prediction = model.predict(input_data)[0]
     confidence = model.predict_proba(input_data)[0][prediction]
 
-    if prediction == 1 and confidence > 0.75:
+    if prediction == 1 and confidence > 0.85:
         label = "Fraud"
         fr_type = "Unsafe Transaction"
         row_exists = ((df.iloc[:, 2:20] == data).all(axis=1)).any()
@@ -58,7 +66,7 @@ async def predict(data: Transaction_data):
             print("Row is not present in the dataset so updating the csv file.")
             changes_in_dataset(label, data)
 
-    elif confidence > 0.55 and confidence < 0.75:
+    elif confidence > 0.65 and confidence < 0.85:
         label = "Non - Fraud"
         fr_type = "Mildly Unsafe Transaction"
 
@@ -66,16 +74,18 @@ async def predict(data: Transaction_data):
             label = "Fraud"
             fr_type = "Unsafe Transaction"
             df2 = df2[df2["IDs"] != acc_holder]
-            df2.to_csv("dataset/mildly_unsafe_transactions.csv", index=False)
+            df2.to_csv(r"dataset/mildly_unsafe_transactions.csv", index=False)
             changes_in_dataset(label, data)
         else:
             print("Putting the mildly unsafe transaction into monitoring dataset.")
             new_row2 = [len(df2)] + [acc_holder] + [datetime.now().strftime("%d-%m-%Y %H:%M:%S")]
             df2.loc[len(df2)] = new_row2
-            df2.to_csv("dataset/mildly_unsafe_transactions.csv", index=False)
+            df2.to_csv(r"dataset/mildly_unsafe_transactions.csv", index=False)
     else:
         label = "Non - Fraud"
         fr_type = "Safe Transaction"
+    
+    print(f"Confidence: {confidence*100:.2f}%")
 
     return {
         "prediction": label,
--- retrain.py
+++ retrain.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import classification_report
@@ -9,18 +10,17 @@ import warnings
 warnings.filterwarnings("ignore")
 
 df = pd.read_csv(r"dataset\cleaned_dataset.csv")
+df = df.replace(r'^\s*$', np.nan, regex=True)
 
-df['ERC20 most sent token type'] = df['ERC20 most sent token type'].fillna('missing')
+df['ERC20 most sent token type'] = df['ERC20 most sent token type'].fillna('missing').astype(str)
 enc1 = LabelEncoder()
 df['ERC20 most sent token type'] = enc1.fit_transform(df['ERC20 most sent token type'])
 
-df['ERC20_most_rec_token_type'] = df['ERC20_most_rec_token_type'].fillna('missing')
+df['ERC20_most_rec_token_type'] = df['ERC20_most_rec_token_type'].fillna('missing').astype(str)
 enc2 = LabelEncoder()
 df['ERC20_most_rec_token_type'] = enc2.fit_transform(df['ERC20_most_rec_token_type'])
 
 df = df.fillna(df.median())
-
-# print(df.isnull().sum())
 
 x = df.iloc[:,2:20]
 y= df.iloc[:,1]
@@ -29,7 +29,7 @@ balancingf = y.value_counts()[0]/y.value_counts()[1]
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.30, shuffle=True , random_state=42, stratify=y)
 
-model = XGBClassifier(scale_pos_weight=balancingf)
+model = XGBClassifier(scale_pos_weight=balancingf, random_state=42)
 model.fit(x_train, y_train)
 
 y_pred = model.predict(x_test)
@@ -38,10 +38,6 @@ print("\t\tClassification Report:\n")
 print(classification_report(y_test, y_pred))
 print(f"\nAccuracy of model : {model.score(x_test, y_test)*100:.2f} %")
 
-bundle = {
-    'model': model,
-    'enc1': enc1,
-    'enc2': enc2
-}
+pack = {'model': model,'enc1': enc1,'enc2': enc2}
 
-joblib.dump(bundle , r"model/models.joblib")
+joblib.dump(pack , r"model/models.joblib")
